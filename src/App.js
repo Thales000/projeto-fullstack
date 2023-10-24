863d5eff49eba88c6f6c1d5ea864b3713900c607
Thales <thalesondas0@gmail.com>
2023-10-24 12:14:49 -0300

    toggle-buttons e escolha por attr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Button, Form, Table, Alert, Image } from 'react-bootstrap';
+import { Button, Form, Table, Alert, Image, Container, Row, Col, ToggleButton} from 'react-bootstrap';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -8,9 +8,16 @@ import Header from './Header';
 function App() {
   const nameInputRef = useRef(null);
   const [inputSearch, setInputSearch] = useState('');
+  const [selectedAttribute, setSelectedAttribute] = useState('');
   const [originalData, setOriginalData] = useState([]);
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
+  const [isHeroChecked, setIsHeroChecked] = useState(true);
+  const [isHeroSearchDisabled, setIsHeroSearchDisabled] = useState(false);
+  const [isHeroButtonDisabled, setIsHeroButtonDisabled] = useState(false);
+  const [isAttrChecked, setIsAttrChecked] = useState(false);
+  const [isAttrSearchDisabled, setIsAttrSearchDisabled] = useState(true);
+  const [isAttrButtonDisabled, setIsAttrButtonDisabled] = useState(true);
 
   useEffect(() => {
     nameInputRef.current.focus();
@@ -28,11 +35,11 @@ function App() {
   const handleInputKeyPress = (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      handleSearch();
+      handleHeroSearch();
     }
   };
 
-  const handleSearch = () => {
+  const handleHeroSearch = () => {
     if (inputSearch.length < 2) {
       setError('Para dados mais específicos, digite pelo menos 2 letras');
       setData(originalData);
@@ -42,6 +49,26 @@ function App() {
     setError(null);
     fetchData();
   };
+
+  const handleHeroChecked = () => {
+    setIsHeroChecked(true);
+    setIsAttrChecked(false);
+    setIsHeroSearchDisabled(false);
+    setIsHeroButtonDisabled(false);
+    setIsAttrSearchDisabled(true);
+    setIsAttrButtonDisabled(true);
+  }
+
+  const handleAttrChecked = () => {
+    setInputSearch('');
+    setError(null);
+    setIsHeroChecked(false);
+    setIsAttrChecked(true);
+    setIsAttrSearchDisabled(false);
+    setIsAttrButtonDisabled(false);
+    setIsHeroSearchDisabled(true);
+    setIsHeroButtonDisabled(true);
+  }
 
   const fetchData = () => {
     fetch('https://api.opendota.com/api/heroStats')
@@ -59,33 +86,91 @@ function App() {
       });
   };
 
+  const fetchDataAttr = () => {
+    fetch('https://api.opendota.com/api/heroStats')
+      .then(resp => resp.json())
+      .then(data => {
+        if(selectedAttribute === '-'){
+          setData(originalData);
+          return
+        }
+        const filteredData = data.filter(attr => attr.primary_attr.includes(selectedAttribute));
+        setData(filteredData);
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      });
+  }
+
   return (
     <div className="App">
       <Header />
       <main className="App-header">
-        <Form>
-          <Form.Group>
-            <Form.Label>Heroi</Form.Label>
-            <Form.Control
-              type='text'
-              size='lg'
-              placeholder='Escolha o nome do heroi'
-              value={inputSearch}
-              onChange={(e) => setInputSearch(e.target.value)}
-              onKeyDown={handleInputKeyPress}
-              ref={nameInputRef}
-            />
-          <Button className='mt-3' onClick={handleSearch}>Pesquisar</Button>
-          {error && <Alert className='mt-3 alert-sm' variant="danger">{error}</Alert>}
-          </Form.Group>
-        </Form>
+        <Container>
+          <Form className='mt-4'>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <ToggleButton
+                    id="toggle-check-hero"
+                    type="checkbox"
+                    variant="outline-danger"
+                    checked={isHeroChecked}
+                    value="1"
+                    onChange={handleHeroChecked}
+                  >
+                    Escolher por heroi
+                  </ToggleButton>
+                  <Form.Control
+                    type='text'
+                    size='lg'
+                    className='mt-3'
+                    placeholder='Escolha o nome do heroi'
+                    value={inputSearch}
+                    onChange={(e) => setInputSearch(e.target.value)}
+                    onKeyDown={handleInputKeyPress}
+                    ref={nameInputRef}
+                    disabled={isHeroSearchDisabled}
+                  />
+                  <Button variant='danger' className='mt-4' onClick={handleHeroSearch} disabled={isHeroButtonDisabled}>Pesquisar</Button>
+                  {error && <Alert className='mt-4 alert-sm' variant="danger">{error}</Alert>}
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <div></div>
+              </Col>
+
+              <Col>
+                <ToggleButton
+                  id="toggle-check-attr"
+                  type="checkbox"
+                  variant="outline-danger"
+                  checked={isAttrChecked}
+                  value="1"
+                  onChange={handleAttrChecked}
+                >
+                  Escolher por atributo
+                </ToggleButton>
+                <Form.Select className='mt-3' size='lg' aria-label="Default select example" onChange={(e) => setSelectedAttribute(e.target.value)} disabled={isAttrSearchDisabled} >
+                  <option value="-">Escolha o atributo do heroi</option>
+                  <option value="all">Todos (all)</option>
+                  <option value="str">Força (str)</option>
+                  <option value="agi">Agilidade (agi)</option>
+                  <option value="int">Inteligência (int)</option>
+                </Form.Select>
+                <Button variant='danger' className='mt-4' onClick={fetchDataAttr} disabled={isAttrButtonDisabled} >Pesquisar</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
         
         <Table variant='dark' className='mt-5' striped bordered hover>
           <thead>
             <tr>
-              <th>Ícone</th>
+              <th>Imagem</th>
               <th>Nome</th>
-              <th>Attributo Principal</th>
+              <th>Atributo Principal</th>
               <th>Ataque</th>
             </tr>
           </thead>
